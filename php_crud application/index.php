ce5e71112b8e94fcf7a8e2597e581fd75ee74a12
Jagadeesh <47549598+jagadeeshgo@users.noreply.github.com>
2021-06-11 22:05:41 +0530

    Update index.php

--- php_crud application/index.php
+++ php_crud application/index.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Home Page</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -24,6 +24,7 @@
 <br><br>
 <div class="container">
   <h2>Home</h2>
+  <p>Consider this as a home page<p><br>
   <a href="logout.php" class="btn btn-primary">log out</a>
 
 </body>
